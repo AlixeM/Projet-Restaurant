24dd24fc711641c2a550e6e0772133c87e0ffe33
AlixeM <alixe.maerte@gmail.com>
2023-11-15 17:50:42 +0100

    PROUT

--- src/restaurant.java
+++ src/restaurant.java
@@ -1,4 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class restaurant {
+class Restaurant {
+    private List<Table> tables;
+    private List<Employee> employees;
+    private List<MenuItem> menu;
+    private Stock stock;
 
+    public Restaurant() {
+        this.tables = new ArrayList<>();
+        this.employees = new ArrayList<>();
+        this.menu = new ArrayList<>();
+        this.stock = new Stock();
+    }
+
+    // Méthode pour gérer l'arrivée d'un groupe de clients
+    public void accueillirClients(GroupOfCustomers group) {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour enregistrer la commande
+    public void enregistrerCommande(Table table, Order order) {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour préparer et servir les commandes
+    public void preparerEtServirCommandes() {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour générer l'addition et gérer le paiement
+    public void gererPaiement(Table table) {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour planifier les employés et reconstituer les stocks avant l'ouverture
+    public void planifierJournee(Manager manager) {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour effectuer des transactions jusqu'à la fermeture
+    public void effectuerTransactions() {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour nettoyer le restaurant
+    public void nettoyerRestaurant() {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour imprimer une liste de courses en fonction des ventes
+    public void imprimerListeCourses(Manager manager) {
+        // Implémenter la logique ici
+    }
+
+    // Méthode pour fournir des outils de monitoring au manager
+    public void monitoringDesPerformances(Manager manager) {
+        // Implémenter la logique ici
+    }
+}
+
+class Table {
+    // Implémenter la classe Table ici
+}
+
+class Employee {
+    // Implémenter la classe Employee ici
+}
+
+class MenuItem {
+    // Implémenter la classe MenuItem ici
+}
+
+class Stock {
+    // Implémenter la classe Stock ici
+}
+
+class GroupOfCustomers {
+    // Implémenter la classe GroupOfCustomers ici
+}
+
+class Order {
+    // Implémenter la classe Order ici
+}
+
+class Manager {
+    // Implémenter la classe Manager ici
 }
