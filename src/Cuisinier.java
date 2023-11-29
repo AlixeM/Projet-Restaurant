b65c2da7edef1caa93a2800e904755a41850c75a
Alexandre <alexandre.deberdt@student.junia.com>
2023-11-29 17:07:48 +0100

    LET HIM COOK (nan c'est un test)

--- src/Cuisinier.java
+++ src/Cuisinier.java
@@ -4,5 +4,7 @@ public class Cuisinier extends Employe {
     int soirDeSuite;
     List<List<Ingredient>> platCommandes;
     //... autres attributs et méthodes
+    //c'est un test 
+    //LET HIM COOK
 
    
