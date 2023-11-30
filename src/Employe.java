768cb8927d35023cb88f92e02b21775e4ac42be0
Alexandre <alexandre.deberdt@student.junia.com>
2023-11-30 12:21:53 +0100

    manager et employé

--- src/Employe.java
+++ src/Employe.java
@@ -3,4 +3,9 @@ public class Employe {
     String prenom;
     double salaire;
     //... autres attributs et méthodes
+    
+    public void nettoyerRestaurant() {
+    	System.out.println("Le restaurant a bien été nettoyé, t'as fait du bon travail chakal");
+    }
+    
 }
--- src/Manager.java
+++ src/Manager.java
@@ -4,4 +4,12 @@ public class Manager extends Employe {
 	public Employe gererEquipe(List <Employe> Employes){
 	//il faut retourner la liste des employés dispos
 	}
+	
+	public void afficherStocks(List <Ingredient> stockPlat, List <Boisson> stockBoisson) {
+	//il faut afficher les ingrédients et le nombre de chacun en stock
+	}
+	
+	public Employe equipeDuJour (List <Employe> EmployesDispos) {
+	//il faut retourner la liste des employés qui travailleront dans la journée
+	}
 }
