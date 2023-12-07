e6beabebe953b47c10089dfa0d800293fadb7978
RaphSoko <raphael.sokolowski@student.junia.com>
2023-12-07 12:43:35 +0100

    Update des employés et de Restaurant

--- src/Employe.java
+++ src/Employe.java
@@ -2,5 +2,12 @@ public class Employe {
     String nom;
     String prenom;
     double salaire;
-    //... autres attributs et méthodes
+    int soirDeSuite;
+    
+    public Employe(String nom, String prenom, double salaire, int soirDeSuite) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.salaire = salaire;
+        this.soirDeSuite = soirDeSuite;    
+        }
 }
