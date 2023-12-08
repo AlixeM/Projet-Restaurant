4997ebb83722722131b66c1b9cfad7e116b8f592
Alexandre <alexandre.deberdt@student.junia.com>
2023-12-08 17:52:37 +0100

    Main écran manager et Transaction commentaires
    pas fini

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,33 @@ public class Main {
             case 4:
                 // Ecran Monitoring
                 // Logique spécifique à l'écran Monitoring
+            	
+            	Scanner scanner2 = new Scanner(System.in);
+                System.out.println("Que souhaitez-vous faire ?");
+                System.out.println("1- Liste des Employés de la journée");
+                System.out.println("2- Liste des courses");
+                System.out.println("3- Performances du restaurant");
+                System.out.println("4- Stocks");
+                int choixEcranManager = scanner2.nextInt();
+                System.out.println("Vous avez choisi l'écran: " + choixEcranManager);
+                
+                switch (choixEcranManager) {
+                	case 1:
+                		//Manager.gererEquipe(List<Employe> employes);
+                		//afficher la liste des employés de la journée
+                		break;
+                	case 2 : 
+                		//Manager.listeDesCourses(Map<Ingredients, Integer> stock);
+                		//afficher la liste des courses à la fin de la journée
+                		break;
+                	case 3 : 
+                		//Manager.performancesJournee();
+                		//afficher le nombre de commandes et l'argent gagné de la journée
+                		break;
+                	case 4 : 
+                		//Afficher la liste des stocks;
+                		break;
+                }
                 break;
 
             default:
--- src/Transaction.java
+++ src/Transaction.java
@@ -6,6 +6,10 @@ public class Transaction {
 	double prix;
 	List <Plat> plats;
 	
+	//ajouter un constructeur qui, pour chaque transaction, ajoute le prix de celle-ci à
+	//un compteur dans Manager pour les performances de la journée et incrémente un autre
+	//compteur du nombre de plats achetés dans la journée
+	
 	public void ajouterPlat(Plat plat) {
 		plats.add(plat);
 	}
