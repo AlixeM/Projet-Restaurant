fdfec0c0460b70ece8737b9f1221a6c61a98d5e6
Alexandre <alexandre.deberdt@student.junia.com>
2023-12-09 18:00:51 +0100

    Table constructeur

--- src/Table.java
+++ src/Table.java
@@ -1,6 +1,27 @@
 import java.util.*;
 
 public class Table {
-	int numero;
-	int nbPersonnes;
+    int numero;
+    int nbPersonnes;
+    int nbPersonnesMax;
+
+    public Table(int numero, int nbPersonnesMax) {
+        this.numero = numero;
+        this.nbPersonnesMax = nbPersonnesMax;
+
+        if (nbPersonnesMax <= 0) {
+            // Si on met pas le bon nombre de personnes max
+            System.out.println("Erreur : Le nombre maximum de personnes doit être supérieur à zéro.");
+            return; // Annule la création de l'instance de Table
+        }
+
+        if (nbPersonnes > nbPersonnesMax) {
+            // Si le nombre de personnes dépasse le max
+            System.out.println("Erreur : Le nombre de personnes dépasse la capacité maximale de la table.");
+            return; // Annule la création de l'instance 
+        }
+
+        // Si tout est bon, initialise le nb de personnes
+        this.nbPersonnes = nbPersonnes;
+    }
 }
